127878bfc57bd50a786c27b16412e0e7475135be
Pablo0925 <pablosumba2000@gmail.com>
2022-09-12 13:20:46 -0500

    Ruta a seleccion de estudiantes y tabla para el proceso de seleccion(Solo modelo)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProcesoSeleccionComponent } from './proceso-seleccion/proceso-seleccion.component';
 import { HeaderComponent } from './Header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes=[
+  {path: '', redirectTo:'',pathMatch:'full'},
+  {path: 'procesoSeleccion', component: ProcesoSeleccionComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProcesoSeleccionComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProcesoSeleccionComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
     
   ],
   providers: [],
